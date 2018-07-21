05209f147bb49407d8ff1da435c1047acb3daa0d
Nogalski, Konrad (EXT) <konrad.nogalski@skanska.pl>
2018-07-21 20:31:42 +0200

    Adding Missing Enum Files

---
+++ ExchangeRateCalculator/ExchangeRateCalculator/Enums/ExchangeRateType.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace SettlementExchangeRateCalculator.Enums
+{
+   
+    public enum ExchangeRateType
+    {
+        Positive,
+        Negative
+    }
+
+    public static class ExchangeRateTypeExtension
+    {
+        private static string positiveExchangeRateText = "Dodatnia";
+        private static string negativeExchangeRateText = "Ujemna";
+        
+        public static string GetStringValue(this ExchangeRateType exchangeRateType)
+        {
+            switch (exchangeRateType)
+            {
+                case ExchangeRateType.Positive: return positiveExchangeRateText;
+                case ExchangeRateType.Negative: return negativeExchangeRateText;
+                default: throw new ArgumentOutOfRangeException($"Brak wartości {nameof(exchangeRateType)} dla typu wyliczeniowego {nameof(ExchangeRateType)}.");
+            }
+        }
+
+        public static ExchangeRateType ToExchangeRateTypeEnum(this string exchangeRateTypeString)
+        {
+            switch (exchangeRateTypeString)
+            {
+                case "Dodatnia": return ExchangeRateType.Positive;
+                case "Ujemna": return ExchangeRateType.Negative;
+                default: throw new ArgumentOutOfRangeException($"Brak wartości {exchangeRateTypeString} dla typu wyliczeniowego {nameof(ExchangeRateType)}.");
+            }
+        }
+    }
+}
---
+++ ExchangeRateCalculator/ExchangeRateCalculator/Enums/SettlementType.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SettlementExchangeRateCalculator.Enums
+{
+    public enum SettlementType
+    {
+        Receivable,
+        Liability
+    }
+
+    public static class SettlemenTypesExtension
+    {
+        public static string GetStringValue(this SettlementType settlementType)
+        {
+            switch (settlementType)
+            {
+                case SettlementType.Receivable: return "Należność";
+                case SettlementType.Liability: return "Zobowiązanie";
+                default: throw new ArgumentOutOfRangeException($"Brak wartości {nameof(settlementType)} dla typu wyliczeniowego {nameof(SettlementType)}.");
+            }
+        }
+
+        public static SettlementType ToSettlementTypeEnum(this string settlementTypeString)
+        {
+            switch (settlementTypeString)
+            {
+                case "Należność": return SettlementType.Receivable;
+                case "Zobowiązanie": return SettlementType.Liability;
+                    default: throw new ArgumentOutOfRangeException($"Brak wartości {settlementTypeString} dla typu wyliczeniowego {nameof(SettlementType)}.");
+            }
+        }
+    }
+}
